8e2ba8e43b4e5cd6abc319932be183de7088355e
Shawn DelPercio <sdelperc@gmail.com>
2020-01-21 16:00:18 -0500

    added imports and useState for app.js, and passed data down from app.js to use in posts

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -14,9 +14,7 @@ const LikeSection = props => {
         <i className="far fa-comment" />
       </div>
     </div>
-    <p className="like-number">
-      
-      likes</p>
+    <p className="like-number">{props.thisMany} likes</p>
 </div>
   )
 };
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,6 +1,6 @@
 // You will add code in this file
 
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,6 +9,7 @@ import "./Posts.css";
 
 const Post = props => {
   // set up state for the likes
+  const [likes, setLikes] = useState(props.post.likes);
 
   return (
     <div className="post-border">
@@ -25,7 +26,7 @@ const Post = props => {
           src={props.post.imageUrl}
         />
       </div>
-      <LikeSection />
+      <LikeSection thisMany={likes} />
       <CommentSection
         postId={props.post.imageUrl}
         comments={props.post.comments}
